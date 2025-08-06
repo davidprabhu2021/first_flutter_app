c7761c19a2a3c4fbdee6a44661eec4a31189fab3
PRABHU R <85546905+davidprabhu2021@users.noreply.github.com>
2025-08-06 18:52:14 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'register_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Auth Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+      },
+    );
+  }
+}
