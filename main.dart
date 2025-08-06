c7c1506d25e61f0f8c0be466efda6957066d7c2d
PRABHU R <85546905+davidprabhu2021@users.noreply.github.com>
2025-08-06 19:00:56 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login_page.dart';
 import 'register_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // <- Initialize Firebase
   runApp(MyApp());
 }
 
@@ -10,10 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Auth Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Firebase Auth Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: '/login',
       routes: {
         '/login': (context) => LoginPage(),
